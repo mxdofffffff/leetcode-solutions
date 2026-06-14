0f9f8ec68e8f78fdcd978e92e0c6a5ad286ab317
mxdoffffff <madoffik@yandex.ru>
2026-06-15 01:43:06 +0300

    solve: Search Insert Position - binary search O(log n)

---
+++ arrays/insert_position.py
@@ -0,0 +1,16 @@
+nums=[1,3,3,4,5,6]
+target = 5
+def insert_position(nums,target):
+    left = 0
+    right = len(nums)-1
+    while left <= right:
+        mid = (left+right)//2  #index
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+print(insert_position(nums,target))
