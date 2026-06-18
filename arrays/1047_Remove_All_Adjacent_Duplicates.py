45b1c8b9b70ef77582ff28d5b402c715e74e3f7c
mxdoffffff <madoffik@yandex.ru>
2026-06-19 00:38:20 +0300

    solve: 1047 Remove All Adjacent Duplicates In String

---
+++ arrays/1047_Remove_All_Adjacent_Duplicates.py
@@ -0,0 +1,10 @@
+s= "abbaca"
+def remove_duplicates(s):
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    return "".join(stack)
+print(remove_duplicates(s))
