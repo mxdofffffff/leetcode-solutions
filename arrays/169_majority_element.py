7df5cba6a1e517596ab10b930e8434031c67f06b
mxdoff <madoffik@yandex.ru>
2026-06-21 20:21:03 +0500

    solve: 169 Majority Element

---
+++ arrays/169_majority_element.py
@@ -0,0 +1,12 @@
+nums = [4,2,3]
+def majority(nums):
+    sl={}
+    for num in nums:
+        if num in sl:
+            sl[num]+=1
+        else:
+            sl[num] = 1
+    for k,value in sl.items():
+        if value>len(nums)//2:
+            return k
+print(majority(nums))
