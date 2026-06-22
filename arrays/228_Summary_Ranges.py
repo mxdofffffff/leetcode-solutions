0adb80f655158196a9ac1de2dcb0eac98fe7d68d
mxdoff <madoffik@yandex.ru>
2026-06-22 14:03:16 +0500

    fix: 228 Summary Ranges

--- arrays/228_Summary_Ranges.py
+++ arrays/228_Summary_Ranges.py
@@ -1,16 +1,19 @@
-nums = [1,2,3,4,6,7]
+nums = [0,2,4,5,7]
 def summary(nums):
     result=[]
+    if not nums:
+        return []
     start=nums[0]
     for i in range(len(nums)-1):
         if nums[i+1]!=nums[i]+1:
             if start == nums[i]:
-                result.append(nums[-1])
+                result.append(str(start))
             else:
                 result.append(f"{start}->{nums[i]}")
             start = nums[i+1]
+
     if start == nums[-1]:
-        result.append(nums[-1])
+        result.append(str(nums[-1]))
     else:
         result.append(f"{start}->{nums[-1]}")
     return result
