858af626d958926d65636ed89f74931b230298cc
mxdoff <madoffik@yandex.ru>
2026-06-28 23:36:55 +0500

    solve: 141 Linked List Cycle

---
+++ arrays/141_Linked_List_Cycle.py
@@ -0,0 +1,10 @@
+head = [3,2,0,-4]
+def hasCycle(head):
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
